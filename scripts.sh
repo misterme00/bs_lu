16f8e757307e0d9e01d41606599209347dea9b72
m <m@mail.yandex.ru>
2023-11-24 01:18:47 +0600

    first commit

---
+++ scripts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define the aliases
+aliases_to_append=(
+    "alias pac='python3 -m venv venv && source venv/bin/activate'"
+    "alias pa='source venv/bin/activate'"
+    "alias dpa='docker ps -a'"
+    # Add more aliases as needed
+)
+
+# Specify the location of the bashrc file
+bashrc_file=~/.bashrc
+
+# Check if the bashrc file exists
+if [ -e "$bashrc_file" ]; then
+    # Backup the current bashrc file
+    cp "$bashrc_file" "$bashrc_file.bak"
+    
+    # Append aliases to the bashrc file
+    for alias_line in "${aliases_to_append[@]}"; do
+        if ! grep -qF "$alias_line" "$bashrc_file"; then
+            echo "$alias_line" >> "$bashrc_file"
+            echo "Appended: $alias_line"
+        else
+            echo "Alias already exists: $alias_line"
+        fi
+    done
+    
+    # Source the bashrc file to apply changes
+    source "$bashrc_file"
+    
+    echo "Aliases added to $bashrc_file and sourced."
+else
+    echo "Error: $bashrc_file does not exist."
+fi
